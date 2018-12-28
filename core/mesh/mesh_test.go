439940bc503e95bfd3bd7a2b7b054d69deeece69
Galaco <mail@galaco.me>
2018-12-28 12:43:18 +0000

    Covered scene and util, more mesh coverage

--- core/mesh/mesh_test.go
+++ core/mesh/mesh_test.go
@@ -1,25 +1,72 @@
 package mesh
 
-import "testing"
+import (
+	"github.com/galaco/Gource-Engine/core/material"
+	"github.com/galaco/Gource-Engine/core/texture"
+	"reflect"
+	"testing"
+)
 
 func TestNewMesh(t *testing.T) {
-	t.Skip()
+	if reflect.TypeOf(NewMesh()) != reflect.TypeOf(&Mesh{}) {
+		t.Errorf("unexpected type returned for NewMesh. Expected: %s, but received: %s", reflect.TypeOf(&Mesh{}), reflect.TypeOf(NewMesh()))
+	}
 }
 
 func TestMesh_AddLightmapCoordinate(t *testing.T) {
-	t.Skip()
+	sut := Mesh{}
+	expected := []float32{
+		1, 2, 3, 4,
+	}
+	sut.AddLightmapCoordinate(expected...)
+
+	for i := 0; i < len(expected); i++ {
+		if sut.LightmapCoordinates()[i] != expected[i] {
+			t.Error("unexpected lightmap coordinate")
+		}
+	}
 }
 
 func TestMesh_AddNormal(t *testing.T) {
-	t.Skip()
+	sut := Mesh{}
+	expected := []float32{
+		1, 2, 3, 4,
+	}
+	sut.AddNormal(expected...)
+
+	for i := 0; i < len(expected); i++ {
+		if sut.Normals()[i] != expected[i] {
+			t.Error("unexpected normal")
+		}
+	}
 }
 
 func TestMesh_AddTextureCoordinate(t *testing.T) {
-	t.Skip()
+	sut := Mesh{}
+	expected := []float32{
+		1, 2, 3, 4,
+	}
+	sut.AddTextureCoordinate(expected...)
+
+	for i := 0; i < len(expected); i++ {
+		if sut.TextureCoordinates()[i] != expected[i] {
+			t.Error("unexpected texture coordinate")
+		}
+	}
 }
 
 func TestMesh_AddVertex(t *testing.T) {
-	t.Skip()
+	sut := Mesh{}
+	expected := []float32{
+		1, 2, 3, 4,
+	}
+	sut.AddVertex(expected...)
+
+	for i := 0; i < len(expected); i++ {
+		if sut.Vertices()[i] != expected[i] {
+			t.Error("unexpected vertex")
+		}
+	}
 }
 
 func TestMesh_Bind(t *testing.T) {
@@ -35,33 +82,101 @@ func TestMesh_Finish(t *testing.T) {
 }
 
 func TestMesh_GetLightmap(t *testing.T) {
-	t.Skip()
+	sut := Mesh{}
+	expected := &texture.Lightmap{}
+	sut.SetLightmap(expected)
+
+	if expected != sut.GetLightmap() {
+		t.Error("unexpected lightmap applied to mesh")
+	}
 }
 
 func TestMesh_GetMaterial(t *testing.T) {
-	t.Skip()
+	sut := Mesh{}
+	expected := &material.Material{
+		FilePath: "foo.vmt",
+	}
+	sut.SetMaterial(expected)
+
+	if expected != sut.GetMaterial() {
+		t.Error("unexpected material applied to mesh")
+	}
 }
 
 func TestMesh_LightmapCoordinates(t *testing.T) {
-	t.Skip()
+	sut := Mesh{}
+	expected := []float32{
+		1, 2, 3, 4,
+	}
+	sut.AddLightmapCoordinate(expected...)
+
+	for i := 0; i < len(expected); i++ {
+		if sut.LightmapCoordinates()[i] != expected[i] {
+			t.Error("unexpected lightmap coordinate")
+		}
+	}
 }
 
 func TestMesh_Normals(t *testing.T) {
-	t.Skip()
+	sut := Mesh{}
+	expected := []float32{
+		1, 2, 3, 4,
+	}
+	sut.AddNormal(expected...)
+
+	for i := 0; i < len(expected); i++ {
+		if sut.Normals()[i] != expected[i] {
+			t.Error("unexpected normal")
+		}
+	}
 }
 
 func TestMesh_SetLightmap(t *testing.T) {
-	t.Skip()
+	sut := Mesh{}
+	expected := &texture.Lightmap{}
+	sut.SetLightmap(expected)
+
+	if expected != sut.GetLightmap() {
+		t.Error("unexpected lightmap applied to mesh")
+	}
 }
 
 func TestMesh_SetMaterial(t *testing.T) {
-	t.Skip()
+	sut := Mesh{}
+	expected := &material.Material{
+		FilePath: "foo.vmt",
+	}
+	sut.SetMaterial(expected)
+
+	if expected != sut.GetMaterial() {
+		t.Error("unexpected material applied to mesh")
+	}
 }
 
 func TestMesh_TextureCoordinates(t *testing.T) {
-	t.Skip()
+	sut := Mesh{}
+	expected := []float32{
+		1, 2, 3, 4,
+	}
+	sut.AddTextureCoordinate(expected...)
+
+	for i := 0; i < len(expected); i++ {
+		if sut.TextureCoordinates()[i] != expected[i] {
+			t.Error("unexpected texture coordinate")
+		}
+	}
 }
 
 func TestMesh_Vertices(t *testing.T) {
-	t.Skip()
+	sut := Mesh{}
+	expected := []float32{
+		1, 2, 3, 4,
+	}
+	sut.AddVertex(expected...)
+
+	for i := 0; i < len(expected); i++ {
+		if sut.Vertices()[i] != expected[i] {
+			t.Error("unexpected vertex")
+		}
+	}
 }
